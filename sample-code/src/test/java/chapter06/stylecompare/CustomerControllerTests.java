220acb52c03bdbc208f04ea525e2345bcf1af6af
byeongsoon <byeongsoon94@gmail.com>
2022-11-08 18:36:28 +0900

    feat: 단위 테스트 스타일 비교 예제

---
+++ sample-code/src/test/java/chapter06/stylecompare/CustomerControllerTests.java
@@ -0,0 +1,58 @@
+package chapter06.stylecompare;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerControllerTests {
+
+    @Test
+    @DisplayName("많은 공간을 차지하는 상태 검증")
+    public void addingACommentToAnArticle1() {
+        Article sut = new Article();
+        String text = "Comment text";
+        String author = "John Doe";
+        LocalDateTime now = LocalDateTime.of(2022,11,8,0,0);
+
+        sut.addComment(text, author, now);
+
+        assertEquals(1, sut.getCommentsCount());
+        assertEquals(text, sut._comments.get(0).text);
+        assertEquals(author, sut._comments.get(0).author);
+        assertEquals(now, sut._comments.get(0).dateCreated);
+    }
+
+    @Test
+    @DisplayName("검증문에 헬퍼 메서드 사용")
+    public void addingACommentToAnArticle2() {
+        Article sut = new Article();
+        String text = "Comment text";
+        String author = "John Doe";
+        LocalDateTime now = LocalDateTime.of(2022,11,8,0,0);
+        ArticleExtensions helper = new ArticleExtensions();
+
+        sut.addComment(text, author, now);
+
+        helper.shouldContainNumberOfComments(sut,1)
+            .withComment(sut, text, author,now);
+    }
+
+    @Test
+    @DisplayName("값으로 비교하는 Comment")
+    public void addingACommentToAnArticle3() {
+        Article sut = new Article();
+        Comment comment = new Comment(
+            "Comment text",
+            "John Doe",
+            LocalDateTime.of(2022,11,8,0,0)
+        );
+
+        sut.addComment(comment.text, comment.author, comment.dateCreated);
+
+        assertEquals(comment, sut._comments.get(0));
+    }
+
+}
