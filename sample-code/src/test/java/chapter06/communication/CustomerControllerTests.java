47407bf59d07bdacfc72cede48b25ff2e452387e
byeongsoon <byeongsoon94@gmail.com>
2022-11-08 17:09:49 +0900

    feat: 통신 기반 테스트 예제

--- sample-code/src/test/java/chapter06/communication/CustomerControllerTests.java
+++ sample-code/src/test/java/chapter06/communication/CustomerControllerTests.java
@@ -1,2 +1,24 @@
-package chapter06.communication;public class CustomerControllerTests {
+package chapter06.communication;
+
+import chapter05.mockandstub.Controller;
+
+import chapter05.mockandstub.IEmailGateway;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class CustomerControllerTests {
+
+    @Test
+    @DisplayName("예제 6.3 통신 기반 테스트")
+    public void sendingGreetingsEmail() {
+        IEmailGateway emailGatewayMock = mock(IEmailGateway.class);
+        Controller sut = new Controller(emailGatewayMock);
+
+        sut.greetUser("user@email.com");
+
+        verify(emailGatewayMock, times(1)).sendGreetingsEmail("user@email.com");
+    }
+
 }
